e7e4b35756087bba52da600d85940bb0512423e0
t19960804 <t19960804@gmail.com>
2019-01-09 11:14:23 +0800

    Initial Commit

---
+++ TwitchTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitchTest
+//
+//  Created by t19960804 on 2019/1/9.
+//  Copyright © 2019 t19960804. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
